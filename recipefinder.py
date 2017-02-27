5f9daf4670a6ff493f29d78962ac84a5b9fb458c
lukaszdygon <lukasz@eduroam-243-195.nomadic.bris.ac.uk>
2017-02-27 14:23:31 +0000

    approximate text matching in recipe finder

--- recipefinder.py
+++ recipefinder.py
@@ -1,15 +1,28 @@
 import pandas as pd
+import difflib
+
+
 recipes = pd.read_csv('recipes.csv')
-def findIngredients(recipe):
-    matches = recipes[recipes['title'].str.contains(recipe)]
-    for values in matches.iloc[0]:
-        
+
+
+def findIngredients(recipe, similar=False):
+    try:
+        recipe = difflib.get_close_matches(recipe, recipes['title'], cutoff=0.5)[0]
+    except:
+        return False
+
+    if similar:
+        matches = recipes[recipes['title'].str.contains(recipe)]
+    else:
+        matches = recipes[recipes['title'] == recipe]
+
+    print(matches)
+    return matches
+
 
 def main():
-    recipes.fillna(True)
-    findIngredients('Tomato-Onion Topping')
-
-
+    recipes.replace('nan', 0)
+    findIngredients('Fried Chicken', True)  # test
 
 
 if __name__ == "__main__":
