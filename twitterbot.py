51543903aa7e0ed361a53c2c4646c520efc9a16d
Ellie Birbeck <elliebirbeck@hotmail.co.uk>
2017-02-27 14:50:21 +0000

    remove while loop

--- twitterbot.py
+++ twitterbot.py
@@ -1,27 +1,26 @@
 import tweepy
 import time
 from credentials import *
-from time import sleep
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 print("Bot started.")
 
-while True:
-    for tweet in tweepy.Cursor(api.search, q='@ratemyplate_').items():
-        try:
-            username = tweet.user.screen_name
-            text = tweet.text
-            location = tweet.user.location
-            api.update_status('@' + username + ' here is an autoreply: ' + str(tweet.id), in_reply_to_status_id=tweet.id)
-            print("Tweet received from ", username, text )
-        except tweepy.TweepError as e:
-            print(e.reason)
 
-        except StopIteration:
-            break
+for tweet in tweepy.Cursor(api.search, q='@ratemyplate_').items():
+    try:
+        username = tweet.user.screen_name
+        text = tweet.text
+        location = tweet.user.location
+        api.update_status('@' + username + ' here is an autoreply: ' + str(tweet.id), in_reply_to_status_id=tweet.id)
+        print("Tweet received from ", username, text )
+    except tweepy.TweepError as e:
+        print(e.reason)
 
-    time.sleep(5)
+    except StopIteration:
+        break
 
 
+print("Bot finished.")
