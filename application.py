2bb8f1963671b4111405df7400fad266084f8d5f
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-02-28 03:14:53 +0000

    It's alive!

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import pandas as pd
 import recipefinder
 import sys
 import food_loc_finder
+import json
 # print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
@@ -24,8 +25,9 @@ def get_recipe_breakdown():
     ingredients = recipefinder.findIngredients(recipe_name,recipes,True)
     countries = finder.get_producers_for_recipe(ingredients, 826)
     producers = [x[1] for x in countries]
-    country_codes = get_country_codes(producers)
-    return render_template("index.html", recipe=recipe_name, ingredients=ingredients, producers=producers, locations=country_codes)
+    country_codes = [x[0] for x in countries]
+    locations = get_locations(country_codes)
+    return render_template("index.html", recipe=json.dumps(recipe_name), ingredients=json.dumps(ingredients), producers=json.dumps(producers), locations=json.dumps(locations))
 
 @application.route('/icons/<path:path>')
 def send_icon(path):
@@ -36,12 +38,12 @@ def send_meal_breakdown(path):
     return send_from_directory('static', path)
 
 
-def get_country_codes(producers):
+def get_locations(producers):
     locations = []
     for producer in producers:
         match = country_locations.loc[country_locations['country'] == producer]
         locations.append((match['latitude'].item(), match['longitude'].item()))
-
+    return locations
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
