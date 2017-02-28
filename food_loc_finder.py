a657d5ff90d940a73f92b6c62336b26218c75e77
Matthew Duffin <matthewjduffin@gmail.com>
2017-02-28 00:46:16 +0000

    Added support for lists of ingredients

--- food_loc_finder.py
+++ food_loc_finder.py
@@ -1,36 +1,44 @@
 import csv
 import pandas as pd
 import comtrade_api
-import difflib
+from fuzzywuzzy import process
+import re
+from nltk.stem.lancaster import LancasterStemmer
  
 class FoodLocationFinder:
     _comtrade = comtrade_api.ComtradeAPI()
     
     def __init__(self, dataset):
         data = pd.read_json(dataset)
-        l = list(data['results'])
-        self._df = pd.DataFrame(l)
+        self._commodities = list(data['results'])
 
     def get_code(self, food):
-        regex = ' {} '.format(food)
-        result = self._df[self._df['text'].str.contains(regex, case=False)]
-        try:
-            return list(result['id'])
-        except IndexError:
-            print("This product doesn't exist.")
-            raise
+        st = LancasterStemmer()
+        stemmed = st.stem(food)
+        result = [] 
+        
+        for item in self._commodities:
+            #if re.search(stemmed, item['text'], re.IGNORECASE):
+            x = process.extractOne(food, item['text'].split())
+            result.append((x[1], item['id']))
+            
+        
+        result.sort(key=lambda x: x[0], reverse=True) #Sort on 1st value
+        print(result[:10])
+        return [i[1] for i in result[:10]]
             
     
     def get_biggest_producer(self, food, region):
         try:
             codes = self.get_code(food)
         except IndexError:
-            return "Unknown"
+            return (-1, "Unknown")
 
         for c in codes:
             df = self._comtrade.get_data(commodity = c, region = region)
 
             if not df.empty:
+                #print(df.columns.values)
                 #We've found some data to work with
                 df = df[['TradeValue', 'ptTitle', 'ptCode']]
                 df = df.ix[df['ptTitle'] != 'World']
@@ -39,10 +47,18 @@ class FoodLocationFinder:
                 return (result['ptCode'], result['ptTitle'])
 
         #If we get here then none of the codes worked
-        return "Unknown"
+        return (-1, "Unknown")
+    
+    def get_producers_for_recipe(self, ingredients, region):
+        result = []
+        for i in ingredients:
+            result.append(self.get_biggest_producer(i, region))
+        return result
+            
 
 
 if __name__ == "__main__":
     finder = FoodLocationFinder('ingredientsHS.json')
+    ingredients = ['grapes', 'lamb']
 
-    print(finder.get_biggest_producer('apple', 826))
+    print(finder.get_producers_for_recipe(ingredients, 826))
